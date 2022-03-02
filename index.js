8afd402b78fcef041f5513e24364e689620b2537
shiceka <shicekaakhona@gmail.com>
2022-03-02 12:09:16 +0200

    first_kata

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function helloWorld(name){
+ 
+    if( name === "" || name === undefined){
+        return "Hello World";
+    }
+        return "Hello " + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+}
+console.log(helloWorld(undefined));
